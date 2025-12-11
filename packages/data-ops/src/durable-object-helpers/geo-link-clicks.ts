2b81f5f099a7914283a4ca338c581db7feaeafae
bkandh30 <kandhari.bhavya20@gmail.com>
2025-12-11 16:54:20 -0700

    Added geo link durable object helper

---
+++ packages/data-ops/src/durable-object-helpers/geo-link-clicks.ts
@@ -0,0 +1,14 @@
+export { drizzle, DrizzleSqliteDODatabase } from "drizzle-orm/durable-sqlite";
+import { integer, real, sqliteTable } from "drizzle-orm/sqlite-core";
+export { migrate } from "drizzle-orm/durable-sqlite/migrator";
+
+export const geoLinkClicksTable = sqliteTable("geo_link_clicks", {
+	id: integer("id").primaryKey({ autoIncrement: true }),
+	latitude: real("latitude").notNull(),
+	longitude: real("longitude").notNull(),
+	time: integer("time", { mode: "timestamp" }).notNull().default(new Date()),
+	linkId: integer("link_id"),
+	userId: integer("user_id"),
+})
+
+export type GeoLinkClick = typeof geoLinkClicksTable.$inferSelect;
