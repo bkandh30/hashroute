f966c3f87d6bb8ce834da0b98e4a4899e52267bb
bkandh30 <kandhari.bhavya20@gmail.com>
2025-12-11 17:36:32 -0700

    Added links database schema and helpers

---
+++ packages/data-ops/src/zod/links.ts
@@ -0,0 +1,54 @@
+import { z } from "zod";
+
+export const destinationSchema = z.preprocess((obj) => {
+	if (typeof obj === "string") {
+		console.log(obj);
+		return JSON.parse(obj);
+	}
+	return obj;
+}, z.object({
+	default: z.url(),
+}).catchall(z.url()),
+);
+
+export type DestinationsSchemaType = z.infer<typeof destinationSchema>;
+
+export const linkSchema = z.object({
+	linkId: z.string(),
+	accountId: z.string(),
+	name: z.string().min(1).max(100),
+	destinations: destinationSchema,
+	created: z.string(),
+	updated: z.string(),
+});
+
+export const createLinkSchema = linkSchema.omit({
+	created: true,
+	updated: true,
+	accountId: true,
+	linkId: true,
+});
+
+export const cloudflareInfoSchema = z.object({
+	country: z.string().optional(),
+	latitude: z.string().transform((val) => (val ? Number(val) : undefined)).optional(),
+	longitude: z.string().transform((val) => (val ? Number(val) : undefined)).optional(),
+});
+
+export const durableObjectGeoClickSchema = z.object({
+	latitude: z.number(),
+	longitude: z.number(),
+	time: z.number(),
+	country: z.string(),
+});
+
+export const durableObjectGeoClickArraySchema = z.array(
+	durableObjectGeoClickSchema,
+);
+
+export type DurableObjectGeoClickSchemaType = z.infer<typeof durableObjectGeoClickSchema>;
+
+export type CloudflareInfoSchemaType = z.infer<typeof cloudflareInfoSchema>;
+
+export type LinkSchemaType = z.infer<typeof linkSchema>;
+export type CreateLinkSchemaType = z.infer<typeof createLinkSchema>;
